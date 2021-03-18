916a93f258c47e3aaee957f11c0336d0bdfcf0a7
wrasler05 <79529851+wrasler05@users.noreply.github.com>
2021-03-18 17:35:03 -0500

    Create PHPsite1.php

---
+++ PHPsite1.php
@@ -0,0 +1,9 @@
+<html><head></head>
+<body>
+<form action="phpaction.php" method="post">
+ <p>User Name: <input type="text" name="username" /></p>
+ <p>Pass word: <input type="text" name="password" /></p>
+ <p><input type="submit" /></p>
+</form>
+</body>
+</html>
