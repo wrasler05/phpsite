87246fef6452fb8bb4a474b25a0aa751e194cadf
wrasler05 <79529851+wrasler05@users.noreply.github.com>
2021-03-23 21:54:48 -0500

    Update PHPsite1.php

--- PHPsite1.php
+++ PHPsite1.php
@@ -1,10 +1,33 @@
-<DOCTYPE html>
+<?php
+session_start();
+$_SESSION['error'] = $_SESSION['error'] ?? '';
+$_SESSION['username'] = $_SESSION['username'] ?? '';
+$_SESSION['password'] = $_SESSION['password'] ?? '';
+
+?>
+
 <html><head></head>
 <body>
-<form action="phpaction.php" method="post">
- <p>User Name: <input type="text" name="username" /></p>
- <p>Pass word: <input type="text" name="password" /></p>
- <p><input type="submit" /></p>
+<form action="site/phpaction.php" method="post">
+ <p>UserName: <input type="text" name="username" /></p>
+ <p>Password: <input type="text" name="password" /></p>
+ <p><input type="submit" value='Login' /></p>
 </form>
+<br>
+<form action="readme.txt" method="post">
+ <p><input type="submit" value='Click for readme'/></p>
+</form>
+
+<?php
+if ($_SESSION['error'] != '')
+{
+  echo $_SESSION['error'];
+}
+else
+{
+  echo  "";
+}
+ 
+?>
 </body>
 </html>
