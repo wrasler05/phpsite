e1da643d0def97c8d1ffd3d9d61cba767a4ddd07
wrasler05 <79529851+wrasler05@users.noreply.github.com>
2021-05-03 11:28:06 -0500

    Delete phpsite1 directory

--- phpsite1/site/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file = 'newfile.txt';
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= "John Smith\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
-?>
